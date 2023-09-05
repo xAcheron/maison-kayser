6aad8849edd0b9ea23a90c4ce4b966149e2b6c98
xAcheron <yasser.arafat20@gmail.com>
2023-09-05 16:00:35 -0600

    ya jala el css
    El CSS no se instala, se compro de una libreria y se ve bien.

--- resources/views/shared/scripts.blade.php
+++ resources/views/shared/scripts.blade.php
@@ -1,6 +1,7 @@
-	<script src="{{ asset('MaterialBS/js/core/jquery.min.js') }}"></script>
+	{{-- <script src="{{ asset('MaterialBS/js/core/jquery.min.js') }}"></script> --}}
+	<script src="{{ asset('material_pro_2_1_0/assets/js/core/jquery.min.js') }}"></script>
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-
+{{-- 
 	<script src="{{ asset('MaterialBS/js/core/popper.min.js') }}"></script>
 
 	<script src="{{ asset('MaterialBS/js/bootstrap-material-design.min.js') }}"></script>
@@ -63,10 +64,77 @@
 	<script src="{{ asset('MaterialBS/js/plugins/sweetalert2.js') }}"></script>
 
 	<!-- Plugin for Fileupload, full documentation here: https://www.jasny.net/bootstrap/javascript/#fileinput -->
-	<script src="{{ asset('MaterialBS/js/plugins/jasny-bootstrap.min.js') }}"></script>
+	<script src="{{ asset('MaterialBS/js/plugins/jasny-bootstrap.min.js') }}"></script> --}}
 
 	<!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar 
 	<script src="{{ asset('MaterialBS/js/plugins/fullcalendar.min.js') }}"></script>   -->
 
 	<!-- demo init 
 	<script src="{{ asset('MaterialBS/js/plugins/demo.js') }}"></script>-->
+
+	<!-- Aqui pongo las rutas nuevas de este archivo -->
+
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+
+<script src="{{ asset('material_pro_2_1_0/assets/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('material_pro_2_1_0/assets/js/bootstrap-material-design.min.js') }}"></script>
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+
+<!--  Plugin for Date Time Picker and Full Calendar Plugin  -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/moment.min.js') }}"></script>
+
+<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/bootstrap-datetimepicker.min.js') }}"></script>
+
+<!--	Plugin for the Sliders, full documentation here: https://refreshless.com/nouislider/ -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/nouislider.min.js') }}"></script>
+<!--	Plugin for Select, full documentation here: https://silviomoreto.github.io/bootstrap-select -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/bootstrap-selectpicker.js') }}"></script>
+
+<!--	Plugin for Tags, full documentation here: https://xoxco.com/projects/code/tagsinput/  -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/bootstrap-tagsinput.js') }}"></script>
+
+<!--	Plugin for Fileupload, full documentation here: https://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/jasny-bootstrap.min.js') }}"></script>
+
+<!-- Plugins for presentation and navigation  -->
+<script src="{{ asset('material_pro_2_1_0/assets-for-demo/js/modernizr.js') }}"></script>
+
+<!-- Material Kit Core initializations of plugins and Bootstrap Material Design Library -->
+
+<script src="{{ asset('material_pro_2_1_0/assets/js/material-dashboard.js') }}"></script>
+<!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+<script src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js') }}"></script>
+
+<!-- Library for adding dynamically elements -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/arrive.min.js') }}"></script>
+
+<!-- Forms Validations Plugin -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/jquery.validate.min.js') }}"></script>
+
+<!--  Charts Plugin, full documentation here: https://gionkunz.github.io/chartist-js/ -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/chartist.min.js') }}"></script>
+
+<!--  Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard 
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/jquery.bootstrap-wizard.js') }}"></script>-->
+
+<!--  Notifications Plugin, full documentation here: https://bootstrap-notify.remabledesigns.com/    -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/bootstrap-notify.js') }}"></script>
+
+<!-- Vector Map plugin, full documentation here: https://jvectormap.com/documentation/ 
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/jquery-jvectormap.js') }}"></script>-->
+
+<!-- Sliders Plugin, full documentation here: https://refreshless.com/nouislider/
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/nouislider.min.js') }}"></script> -->
+
+<!--  Plugin for Select, full documentation here: https://silviomoreto.github.io/bootstrap-select -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/jquery.select-bootstrap.js') }}"></script>
+
+<!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/jquery.datatables.js') }}"></script>
+
+<!-- Sweet Alert 2 plugin, full documentation here: https://limonte.github.io/sweetalert2/ -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/sweetalert2.js') }}"></script>
+
+<!-- Plugin for Fileupload, full documentation here: https://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="{{ asset('material_pro_2_1_0/assets/js/plugins/jasny-bootstrap.min.js') }}"></script>
--- resources/views/shared/styles.blade.php
+++ resources/views/shared/styles.blade.php
@@ -2,7 +2,8 @@
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
 
-<link rel="stylesheet" href="{{ asset('MaterialBS/css/material-dashboard.min.css') }}"/>
+{{-- <link rel="stylesheet" href="{{ asset('MaterialBS/css/material-dashboard.min.css') }}"/> --}}
+<link rel="stylesheet" href="{{ asset('material_pro_2_1_0/assets/css/material-dashboard.min.css') }}" />
 
 <style>
 
